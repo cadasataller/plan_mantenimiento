73526d3030094814fb868066d6bc060d211d70a3
cadasataller <cadasataller@gmail.com>
2026-03-27 13:19:05 -0500

    abrir form dedse om modal

--- js/components/sg/sg-page.component.js
+++ js/components/sg/sg-page.component.js
@@ -1,6 +1,7 @@
 const SGPageComponent = (() => {
   let _container = null;
   let _currentState = 'list'; // 'list' | 'form_manual'
+  let _formData = {}; // 👈 NUEVO: Guardará los datos pre-llenados si vienen de otra pestaña
 
   function mount(containerId) {
     _container = document.getElementById(containerId);
@@ -9,6 +10,14 @@ const SGPageComponent = (() => {
 
   function onEnter() {
     _currentState = 'list';
+    _formData = {}; // Limpiamos los datos al entrar
+    _render();
+  }
+
+  // 👇 NUEVO: Función para abrir el formulario desde afuera inyectando datos
+  function openForm(initialData = {}) {
+    _formData = initialData;
+    _currentState = 'form_manual';
     _render();
   }
 
@@ -20,25 +29,27 @@ const SGPageComponent = (() => {
     if (_currentState === 'list') {
       SGListComponent.mount('sg-sub-view', {
         onNewManual: () => {
-          _currentState = 'form_manual';
-          _render();
+          openForm({}); // Abrimos el formulario vacío
         }
       });
     } else if (_currentState === 'form_manual') {
       SGFormComponent.mount('sg-sub-view', {
         onCancel: () => {
           _currentState = 'list';
+          _formData = {}; // Limpiamos
           _render();
         },
         onSuccess: () => {
           _currentState = 'list';
+          _formData = {}; // Limpiamos
           _render();
         }
-      });
+      }, _formData); // 👈 Pasamos los datos al formulario
     }
   }
 
-  return { mount, onEnter };
+  // 👈 Exportamos openForm para que el modal de OT pueda usarlo
+  return { mount, onEnter, openForm }; 
 })();
 
 window.SGPageComponent = SGPageComponent;
